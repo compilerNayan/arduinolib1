b83714dfdfa44b97712f2e870ed4748349fea0e8
Nayan Kurude <nkurude@atlassian.com>
2025-12-16 22:12:33 +0530

    Removed unnecessary script.

--- arduinolib1.py
+++
@@ -1,6 +0,0 @@
-Import("env")
-
-# This file uses PlatformIO's library hook mechanism
-# Files named after the library in the root directory are automatically executed
-print("hello i am in arduinolib1")
-
