9c15e61328e4a70b0d6020d433f16c7d87d506f9
Nayan Kurude <nkurude@atlassian.com>
2025-12-16 21:40:14 +0530

    Removed boilerplate

--- scripts/pre_build.py
+++ scripts/pre_build.py
@@ -1,16 +1,2 @@
-Import("env")
-
 # Print message immediately when script is loaded
 print("Hello cool dudes normal")
-
-# Ensure ArduinoJson dependency is installed
-import subprocess
-import os
-
-# Also hook into the build process to ensure it runs
-def before_build(source, target, env):
-    print("Hello cool dudes before build")
-
-# Hook into the build process before compilation starts
-env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", before_build)
-
