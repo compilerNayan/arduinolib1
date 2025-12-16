84679bb6ed68c7cfb573b2acaaf305736f2b28f7
Nayan Kurude <nkurude@atlassian.com>
2025-12-16 14:20:48 +0530

    First commit.

---
+++ src/NayanMath.h
@@ -0,0 +1,19 @@
+#ifndef NAYANMATH_H
+#define NAYANMATH_H
+
+namespace NayanMath {
+
+/**
+ * Adds two integers and returns their sum
+ * @param a First integer
+ * @param b Second integer
+ * @return Sum of a and b
+ */
+inline int add(int a, int b) {
+    return a + b;
+}
+
+} // namespace NayanMath
+
+#endif // NAYANMATH_H
+
